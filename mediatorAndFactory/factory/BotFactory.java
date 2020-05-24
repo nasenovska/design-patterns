ee1bfdfb23a0b7de07b86aa8275d3e614ad2820d
Nina Asenovska <63414692+nasenovska@users.noreply.github.com>
2020-05-24 22:46:54 +0300

    Delete BotFactory.java

--- mediatorAndFactory/factory/BotFactory.java
+++
@@ -1,9 +0,0 @@
-package com.uni.patterns.mediatorAndFactory.factory;
-
-import com.uni.patterns.mediatorAndFactory.participant.Bot;
-
-public class BotFactory {
-    Bot getBot() {
-        return new Bot();
-    }
-}
