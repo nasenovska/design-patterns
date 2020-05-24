e39d5a85af6f3d34dfa1ce72c571d84128392305
Nina Asenovska <63414692+nasenovska@users.noreply.github.com>
2020-05-24 22:46:48 +0300

    Delete UserFactory.java

--- mediatorAndFactory/factory/UserFactory.java
+++
@@ -1,9 +0,0 @@
-package com.uni.patterns.mediatorAndFactory.factory;
-
-import com.uni.patterns.mediatorAndFactory.participant.User;
-
-public class UserFactory {
-    User getUser(String name) {
-        return new User(name);
-    }
-}
